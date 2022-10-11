8869419bd53e9cf8cf3f9dfb0d20d058bd2dacd6
anowarzz <anowarhosen444@gmail.com>
2022-10-11 10:39:48 +0600

    Navlink added to header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog/Blog';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Main from './components/layouts/Main';
+import Statistics from './components/Statistics/Statistics';
+import Topics from './components/Topics/Topics';
+
+
+const router = createBrowserRouter([
+{
+  path:'/',
+  element: <Main />,
+  children: [
+    {
+      path:'/home',
+      element: <Home />,
+    },
+    {
+      path:'/',
+      element: <Home />,
+    },
+    {
+      path:'/topics',
+      element: <Topics />,
+    },
+    {
+      path:'/statistics',
+      element: <Statistics />,
+    },
+    {
+      path:'/blog',
+      element: <Blog />,
+    },
+  ]
+},
+
+
+])
+
 
 function App() {
   return (
     <div >
-   <Header /> 
+   <RouterProvider router={router}> </RouterProvider>
     </div>
   );
 }
