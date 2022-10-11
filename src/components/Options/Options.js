8862b04bdb3b89c654cc20eecfd99a47a38be9e9
anowarzz <anowarhosen444@gmail.com>
2022-10-11 20:58:29 +0600

    radio buttons modified

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -3,12 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Options = ({option, correctAnswer, handleAnswer}) => {
 
-
-
     return (
         <div className='p-8 bg-gray-700 w-5/6'>
        <input  type="radio" name="option" id={option} /> 
-             <label onClick={(e) => handleAnswer(e.target.innerText)} htmlFor="" className='text-xl radio radio-primary pl-3 hover:bg-green-400 rounded py-1 hover:text-black'>{option}</label>
+             <label onClick={(e) => handleAnswer(e.target.innerText)} htmlFor={option} className='text-xl radio radio-primary px-2 hover:bg-green-400 rounded py-1 hover:text-black'>{option}</label>
 
         </div>
     );
